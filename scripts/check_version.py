9e4b88fb6a5d0af77beca9e60d927b14850eb8e6
Yuta Hayashibe <yuta@hayashibe.jp>
2022-10-10 00:27:31 +0900

    Fix operation of /dev/stdin for Windows

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -47,9 +47,13 @@ def main() -> None:
 
     if opts.tags:
         tags = []
-        with opts.tags.open() as f:
-            for line in f:
+        if str(opts.tags) == "/dev/stdin":
+            for line in sys.stdin:
                 tags.append(line[:-1])
+        else:
+            with opts.tags.open() as f:
+                for line in f:
+                    tags.append(line[:-1])
 
         if stable_version not in tags:
             sys.stderr.write(f"Tag {stable_version} not in git tags: {tags}\n")
