a89e9d2ff6c22c8d0fae7d1b9d6d1867412317e8
ricwo <r@cogr.am>
2022-11-11 08:19:58 +0100

    better cmdline printing

--- download_whisper_models.py
+++ download_whisper_models.py
@@ -5,6 +5,8 @@ import whisper
 
 download_path = str(Path("~/.cache/whisper").expanduser())
 
+print(f"Downloading whisper models to {download_path}")
+
 # get models to download from argparse
 parser = argparse.ArgumentParser()
 
@@ -13,5 +15,8 @@ parser.add_argument("--models", nargs="+", default=[])
 
 args = parser.parse_args()
 
+print(f"Downloading models: {args.models}")
+
 for m in args.models:
+    print(f"Downloading model {m} to {download_path}")
     whisper._download(whisper._MODELS[m], download_path, False)
